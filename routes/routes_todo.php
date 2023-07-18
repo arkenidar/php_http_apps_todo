91b29c56830efa6c46942f0666395cedd759014d
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-07-18 09:26:44 +0200

    added alternative routing system, and converted to it existing

--- routes/routes_todo.php
+++ routes/routes_todo.php
@@ -1,30 +1,30 @@
 <?php
 
-$routes['todo_list'] = function($request_variables){
+function routes_todo_list($request_variables){
     require_once 'manager/todo_manager.php';
     todo_render();
-};
+}
 
-$routes['todo_add'] = function($request_variables){
+function routes_todo_add($request_variables){
     require_once 'manager/todo_manager.php';
     todo_add($request_variables['item']);
     header("Location: ?r=todo_list");
-};
+}
 
-$routes['todo_remove'] = function($request_variables){
+function routes_todo_remove($request_variables){
     require_once 'manager/todo_manager.php';
     todo_remove($request_variables['id']);
     header("Location: ?r=todo_list");
-};
+}
 
-$routes['todo_detail'] = function($request_variables){
+function routes_todo_detail($request_variables){
     require_once 'manager/todo_manager.php';
     if(!isset($request_variables['id'])) return;
     $id = (int)$request_variables['id'];
     todo_render_detail($id);
-};
+}
 
-$routes['todo_update'] = function($request_variables){
+function routes_todo_update($request_variables){
     require_once 'manager/todo_manager.php';
     if(!isset($request_variables['id'])) return;
     if(!isset($request_variables['description'])) return;
@@ -32,4 +32,4 @@ $routes['todo_update'] = function($request_variables){
     $description = (string)$request_variables['description'];
     todo_update($id, $description);
     header("Location: ?r=todo_list");
-};
+}
