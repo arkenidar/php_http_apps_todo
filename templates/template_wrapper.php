382e9ed12ae20ae46c64920a1f85042311f7a669
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2023-10-02 12:02:05 +0200

    [language-translation] needs traditional and complete HTML structuring.

--- templates/template_wrapper.php
+++ templates/template_wrapper.php
@@ -1,10 +1,10 @@
-<!-- <!DOCTYPE html><html lang="en"><head> -->
+<!DOCTYPE html><html lang="en"><head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
 
-<!-- </head><body> -->
+</head><body>
 
 <?= $_get_wrapped_content() ?>
 
-<!-- </body></html> -->
+</body></html>
