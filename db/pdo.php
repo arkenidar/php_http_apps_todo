41412292ed7d9ffe90ae7e47dab7acd37e61cf37
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-07-17 00:44:38 +0200

    pdo db connection for mysql

--- db/pdo.php
+++ db/pdo.php
@@ -1,3 +1,4 @@
 <?php
-$db = new PDO('sqlite:db/todo_db.sqlite');
+//$db = new PDO('sqlite:db/todo_db.sqlite');
+$db = new PDO("mysql:host=127.0.0.1;dbname=php_todo;charset=UTF8", "username", "password");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
