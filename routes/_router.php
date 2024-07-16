e528a531d098de5b05e4f475079dada5d39adc08
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-07-16 16:02:52 +0200

    [redirect] relative redirect , not absolute redirect

--- routes/_router.php
+++ routes/_router.php
@@ -5,5 +5,5 @@ require_once 'routes/routes_todo.php';
 // home page redirect to todo_list
 $routes[''] = function ($request_variables) {
     // echo 'Hello World';
-    header('Location: /?r=todo_list');
+    header('Location: ?r=todo_list');
 };
