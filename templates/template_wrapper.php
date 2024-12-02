7d4703e55f13ff9ee1450403b07b468fd977c5de
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-02 23:49:21 +0100

    [ docs ] links to : gh repo and author home page .

--- templates/template_wrapper.php
+++ templates/template_wrapper.php
@@ -3,19 +3,31 @@
 
 <head>
 
+    <!-- title>php http app</title> -->
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
+
+    <!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/appwide.css">
+    -->
 </head>
 
 <body>
 
+    <a href="https://github.com/arkenidar/php_http_apps_todo" target="_blank" >
+        ORIGIN : gh repo clone arkenidar/php_http_apps_todo ( from GitHub ) </a>
+    <hr>
+
     <?= $_get_wrapped_content() ?>
+
+    <hr>
+    <a href="https://arkenidar.com/coder.html" target="_blank" > coder : arkenidar </a>
 
 </body>
 
