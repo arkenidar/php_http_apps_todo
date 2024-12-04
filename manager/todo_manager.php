faca529799aa954ad8f20af5cdbf7a6258fa08cb
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-04 11:36:14 +0100

    refactor: streamline database interactions in todo_manager.php using query functions

--- manager/todo_manager.php
+++ manager/todo_manager.php
@@ -1,10 +1,32 @@
 <?php
 
-function todo_list()
+//-----------------------------------------
+// pdo_utilities.php
+//-----------------------------------------
+
+function queryAll($sql, $params = [])
 {
     require '../db/pdo.php';
-    $items = $db->query('SELECT * FROM todos');
+    $statement = $db->prepare($sql);
+    $statement->execute($params);
+    $items = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $items;
+}
+
+function query($sql, $params = [], $index = 0)
+{
+    $result = queryAll($sql, $params);
+    if (count($result) == 0) return null;
+    return $result[$index];
+}
+
+//-----------------------------------------
+// todo_manager.php
+//-----------------------------------------
+
+function todo_list()
+{
+    return queryAll('SELECT * FROM todos');
 }
 
 function todo_render()
@@ -16,35 +38,26 @@ function todo_render()
 
 function todo_remove($id)
 {
-    require '../db/pdo.php';
-    $stmt = $db->prepare('DELETE FROM todos WHERE id=:id');
-    $stmt->execute([':id' => $id]);
+    query('DELETE FROM todos WHERE id=:id', [':id' => $id]);
 }
 
-function todo_add($item)
+function todo_add($description)
 {
-    $item = trim($item);
-    if ($item == '') return;
-    require '../db/pdo.php';
-    $stmt = $db->prepare('INSERT INTO todos (description) VALUES (:description)');
-    $stmt->execute([':description' => $item]);
+    $description = trim($description);
+    if ($description == '') return;
+
+    query('INSERT INTO todos (description) VALUES (:description)', compact('description'));
 }
 
 function todo_render_detail($id)
 {
-    require '../db/pdo.php';
-    $stmt = $db->prepare('SELECT * FROM todos WHERE id=:id');
-    $stmt->execute([':id' => $id]);
-    $item = $stmt->fetchAll()[0];
+    $template_variables = query('SELECT * FROM todos WHERE id=:id', [':id' => $id]);
 
     require_once '../templates/lib_template.php';
-    echo apply_template('todo/template_todo_detail', $item);
+    echo apply_template('todo/template_todo_detail', $template_variables);
 }
 
 function todo_update($id, $description)
 {
-    require '../db/pdo.php';
-    $stmt = $db->prepare('UPDATE todos SET description=:description WHERE id=:id');
-    /// below is equivalent to : $stmt->execute([':id' => $id, ':description' => $description]);
-    $stmt->execute(compact("id", "description"));
+    query('UPDATE todos SET description=:description WHERE id=:id', compact("id", "description"));
 }
