3d96ea3fb98226aa4265f671d4bb6e637bc39f87
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-09 23:04:03 +0100

    refactor: disable autocomplete for description input in todo detail template for improved user experience

--- templates/todo/template_todo_detail.php
+++ templates/todo/template_todo_detail.php
@@ -10,7 +10,8 @@ $GLOBALS['_title_tag_content'] = 'To-Do List. List item detail.';
 <div>To-Do List. List item detail.</div>
 
 <form method="post" action="?r=todo_update">
-    <input name="description" placeholder="list item" value="<?= $_('description') ?>" autofocus="true">
+    <input name="description" placeholder="list item" value="<?= $_('description') ?>" autofocus="true"
+        autocomplete="off">
     <input type="hidden" name="id" value="<?= $_('id') ?>">
     <input type="submit" value="update">
     <button onclick="location.href = '?r=todo_remove&id=<?= $_('id') ?>' ">remove</button>
