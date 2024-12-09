907de21f2c9754c8c4c4f99e149c9508f68f45b2
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-09 22:05:46 +0100

    refactor: replace remove link with button for better user experience in todo detail template

--- templates/todo/template_todo_detail.php
+++ templates/todo/template_todo_detail.php
@@ -8,10 +8,10 @@ $GLOBALS['_title_tag_content'] = 'To-Do List. List item detail.';
 <title>To-Do List. List item detail.</title>
 
 <div>To-Do List. List item detail.</div>
-<a href="?r=todo_remove&id=<?= $_('id') ?>">Remove</a>
 
 <form method="post" action="?r=todo_update">
     <input name="description" placeholder="list item" value="<?= $_('description') ?>" autofocus="true">
     <input type="hidden" name="id" value="<?= $_('id') ?>">
     <input type="submit" value="update">
+    <button onclick="location.href = '?r=todo_remove&id=<?= $_('id') ?>' ">remove</button>
 </form>
