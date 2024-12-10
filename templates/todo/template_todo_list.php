2c9c106ea167bfd6f7c58b9e751fff3aa06de333
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-10 01:34:28 +0100

    refactor: update todo list template to enhance item display and add checkbox functionality

--- templates/todo/template_todo_list.php
+++ templates/todo/template_todo_list.php
@@ -5,13 +5,32 @@ $_wrap_with('template_wrapper');
 <?php
 $GLOBALS['_title_tag_content'] = 'To-Do List. Listing.';
 ?>
-<title>To-Do List.</title>
 
-<div>To-Do List.</div>
+<div>To-Do List. Listing.</div>
 <form method="post" action="?r=todo_add">
     <input name="item" placeholder="list item" autofocus="true" autocomplete="off">
-    <input type="submit" value="add">
+    <button type="submit">add</button>
 </form>
-<?php foreach ($_u('items') as $item) { ?>
-    <li><a href="?r=todo_detail&id=<?= $_e($item['id']) ?>"><?= $_e($item['description']) ?></a></li>
-<?php } ?>
+<div class="todo_list">
+    <?php foreach ($_u('items') as $item) { ?>
+        <a href="?r=todo_detail&id=<?= $_e($item['id']) ?>">
+            <div><input type="checkbox" class="todo_state" <?= ($item['state'] == 0) ? '' : 'checked' ?>
+                    data-id="<?= $_e($item['id']) ?>">
+                <?= $_e($item['description']) ?>
+            </div>
+        </a>
+    <?php } ?>
+</div>
+<script>
+    document.querySelectorAll('input[type="checkbox"].todo_state').forEach(function(checkbox) {
+        checkbox.addEventListener('change', function(event) {
+            fetch('?r=todo_update_state', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'id=' + this.dataset.id + '&state=' + (this.checked ? 1 : 0)
+            })
+        });
+    });
+</script>
