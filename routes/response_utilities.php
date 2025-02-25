bd5e7bd3d1e321db2dd1ed0b6996a533cb0d6be9
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-02-25 13:19:13 +0100

    refactor: enhance bean handling in response utilities for improved type safety and error handling

--- routes/response_utilities.php
+++ routes/response_utilities.php
@@ -33,11 +33,22 @@ function json_response($data)
     echo json_encode($data, JSON_PRETTY_PRINT);
 }
 
+function is_bean($bean)
+{
+    return is_object($bean) && method_exists($bean, 'export');
+}
+
+// bean to typed array
 function bean_to_typed_array($bean)
 {
+    is_bean($bean) or die("error: not a bean");
+
     $result = [];
     foreach ($bean as $key => $value) {
-        if (is_numeric($value) && strpos($value, '.') !== false) {
+        if (is_bean($value)) {
+            // If the value is a bean, convert it recursively
+            $result[$key] = bean_to_typed_array($value);
+        } else if (is_numeric($value) && strpos($value, '.') !== false) {
             $result[$key] = floatval($value);
         } else if (is_numeric($value)) {
             $result[$key] = intval($value);
@@ -52,19 +63,27 @@ function bean_to_typed_array($bean)
 
 function beans_to_typed_array($beans)
 {
-    $beans_array = R::exportAll($beans);
+    if ($beans['__type'] != "bean_array") {
+        return "error: not a bean array";
+    }
+    $beans_array = array_values($beans);
+    $beans_array = array_filter($beans_array, 'is_bean');
     $array       = array_map('bean_to_typed_array', $beans_array);
     return $array;
 }
 
 function json_response_beans($beans)
 {
-    $array = beans_to_typed_array($beans);
+    $beans['__type'] = "bean_array";
+    $array           = beans_to_typed_array($beans);
     json_response($array);
 }
 
 function json_response_bean($bean)
 {
+    if (! is_bean($bean)) {
+        return;
+    }
     $array = bean_to_typed_array($bean);
     json_response($array);
 }
