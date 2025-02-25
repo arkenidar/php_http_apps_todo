98c96915538dd7383292e0a34493bc250630b57a
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-02-25 02:00:16 +0100

    refactor: consolidate todo retrieval logic and enhance JSON response handling for todos

--- routes/response_utilities.php
+++ routes/response_utilities.php
@@ -32,3 +32,39 @@ function json_response($data)
     header_content_type('application/json');
     echo json_encode($data, JSON_PRETTY_PRINT);
 }
+
+function bean_to_typed_array($bean)
+{
+    $result = [];
+    foreach ($bean as $key => $value) {
+        if (is_numeric($value) && strpos($value, '.') !== false) {
+            $result[$key] = floatval($value);
+        } else if (is_numeric($value)) {
+            $result[$key] = intval($value);
+        } else if ($value === 'true' || $value === 'false') {
+            $result[$key] = $value === 'true';
+        } else {
+            $result[$key] = $value;
+        }
+    }
+    return $result;
+}
+
+function beans_to_typed_array($beans)
+{
+    $beans_array = R::exportAll($beans);
+    $array       = array_map('bean_to_typed_array', $beans_array);
+    return $array;
+}
+
+function json_response_beans($beans)
+{
+    $array = beans_to_typed_array($beans);
+    json_response($array);
+}
+
+function json_response_bean($bean)
+{
+    $array = bean_to_typed_array($bean);
+    json_response($array);
+}
--- routes/routes_todo.php
+++ routes/routes_todo.php
@@ -10,13 +10,25 @@ function routes_todo_list($request_variables)
 {
     require_once '../manager/todo_manager.php';
     todo_render();
-};
+}
 
-function routes_todo_list_json_get($request_variables)
+function routes_todo_list_json($request_variables)
 {
     require_once '../manager/todo_manager.php';
-    json_response(todo_list());
-};
+    $beans = todo_list();
+    json_response_beans($beans);
+}
+
+function routes_todo_get_json($request_variables)
+{
+    if (! isset($request_variables['id'])) {
+        return;
+    }
+    $id = (int) $request_variables['id'];
+    require_once '../manager/todo_manager.php';
+    $bean = todo_get($id);
+    json_response_bean($bean);
+}
 
 function routes_todo_add($request_variables)
 {
@@ -35,18 +47,27 @@ function routes_todo_remove($request_variables)
 function routes_todo_detail($request_variables)
 {
     require_once '../manager/todo_manager.php';
-    if (!isset($request_variables['id'])) return;
-    $id = (int)$request_variables['id'];
+    if (! isset($request_variables['id'])) {
+        return;
+    }
+
+    $id = (int) $request_variables['id'];
     todo_render_detail($id);
 }
 
 function routes_todo_update_description($request_variables)
 {
     require_once '../manager/todo_manager.php';
-    if (!isset($request_variables['id'])) return;
-    if (!isset($request_variables['description'])) return;
-    $id = (int)$request_variables['id'];
-    $description = (string)$request_variables['description'];
+    if (! isset($request_variables['id'])) {
+        return;
+    }
+
+    if (! isset($request_variables['description'])) {
+        return;
+    }
+
+    $id          = (int) $request_variables['id'];
+    $description = (string) $request_variables['description'];
     todo_update_description($id, $description);
     redirect_to('todo_list');
 }
@@ -54,10 +75,16 @@ function routes_todo_update_description($request_variables)
 function routes_todo_update_state($request_variables)
 {
     require_once '../manager/todo_manager.php';
-    if (!isset($request_variables['id'])) return;
-    if (!isset($request_variables['state'])) return;
-    $id = (int)$request_variables['id'];
-    $state = (int)$request_variables['state'];
+    if (! isset($request_variables['id'])) {
+        return;
+    }
+
+    if (! isset($request_variables['state'])) {
+        return;
+    }
+
+    $id    = (int) $request_variables['id'];
+    $state = (int) $request_variables['state'];
     todo_update_state($id, $state);
     redirect_to('todo_list');
 }
