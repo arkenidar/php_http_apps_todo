dec1f67fe4472d7ed1ffc30eb13e6a577098aaea
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-03-09 18:45:43 +0100

    feat: update user creation and login forms with Bootstrap styling

--- templates/user/template_user_create_form.php
+++ templates/user/template_user_create_form.php
@@ -7,17 +7,30 @@ $GLOBALS['_title_tag_content'] = 'Create User';
 ?>
 
 <h2>Create User</h2>
-<form method="post" action="">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
-    <br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    <br>
-    <label for="confirm_password">Confirm Password:</label>
-    <input type="password" id="confirm_password" name="confirm_password" required>
-    <br>
-    <button type="submit">Create User .</button>
+<form method="post" action="" class="form-horizontal">
+    <div class="form-group">
+        <label for="username" class="col-sm-2 control-label">Username:</label>
+        <div class="col-sm-10">
+            <input type="text" id="username" name="username" class="form-control" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password" class="col-sm-2 control-label">Password:</label>
+        <div class="col-sm-10">
+            <input type="password" id="password" name="password" class="form-control" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="confirm_password" class="col-sm-2 control-label">Confirm Password:</label>
+        <div class="col-sm-10">
+            <input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Create User</button>
+        </div>
+    </div>
 </form>
 <div>
     <?php
--- templates/user/template_user_login_form.php
+++ templates/user/template_user_login_form.php
@@ -7,14 +7,24 @@ $GLOBALS['_title_tag_content'] = 'Login';
 ?>
 
 <h2>Login</h2>
-<form method="post" action="router.php?r=user_login_submit">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
-    <br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    <br>
-    <button type="submit">Login .</button>
+<form method="post" action="router.php?r=user_login_submit" class="form-horizontal">
+    <div class="form-group">
+        <label for="username" class="col-sm-2 control-label">Username:</label>
+        <div class="col-sm-10">
+            <input type="text" id="username" name="username" class="form-control" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password" class="col-sm-2 control-label">Password:</label>
+        <div class="col-sm-10">
+            <input type="password" id="password" name="password" class="form-control" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+    </div>
 </form>
 <div>
     <?php
