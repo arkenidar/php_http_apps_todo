a6e008ead7a897136f8d57ac88cd9e2127350c4d
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2026-01-12 11:10:41 +0100

    [rb-sqlite] use

--- db/red-bean-orm-use.php
+++ db/red-bean-orm-use.php
@@ -1,5 +1,5 @@
 <?php
 $current_directory = dirname(__FILE__) . '/';
-require_once $current_directory . 'red-bean-orm.php';
+require_once $current_directory . 'rb-sqlite.php';
 R::setup('sqlite:' . $current_directory . 'todo_db.sqlite');
 // R::setup('pgsql:host=127.0.0.1;port=5432;dbname=database1;', 'postgres', 'password');
